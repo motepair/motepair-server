f6e7adc8b750d73a0c60e2e90e2b00664cc56c46
Luiz Filho <luizbafilho@gmail.com>
2014-12-31 17:24:03 +0000

    Adding port variable

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var WsEmitterServer = require("./ws-emit-server.js");
 
-var sm = new WsEmitterServer(3000);
+var port = process.env.PORT || 3000
+
+var sm = new WsEmitterServer(port);
 
 sm.on("connection", function(conn){
   console.log("user connected")
-
+  
   conn.on("change", function(message){
     conn.broadcast("change", message)
   });

d1cad3f4e6d483825ac9c1939ca17cc31880bebb
Joe Constant <joe@joeconstant.com>
2015-12-03 19:00:17 -0700

    Only enable NewRelic agent if license key set

--- target/newrelic.js
+++ target/newrelic.js
@@ -13,6 +13,7 @@ exports.config = {
    * Your New Relic license key.
    */
   license_key : process.env.NEW_RELIC_LICENSE_KEY,
+  agent_enabled: (process.env.NEW_RELIC_LICENSE_KEY != null),
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
